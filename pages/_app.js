fd3eaa1a15dadaff11fb1a8b55726897ab4c7c90
Ishaq-H <itzizak@hotmail.com>
2023-09-25 18:51:07 +0100

    homepage

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCrown, faStar, faStarHalf } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCrown, faStar, faStarHalf);
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
