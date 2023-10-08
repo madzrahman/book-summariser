7af896d75b5be24d08f153f42932dca772abd4b7
Ishaq-H <itzizak@hotmail.com>
2023-10-08 19:06:47 +0100

    tags

--- pages/book/[id].jsx
+++ pages/book/[id].jsx
@@ -37,18 +37,18 @@ export default function BookInfo() {
         <div className="row">
           {/* <audio src=""></audio> Add this later I guess */}
           <div className="container">
-            <div className="flex gap-[16px]">
-              <div className="w-full">
-                <div className="text-[#032b41] mb-[16px] font-[600] text-[32px]">
+            <div className="flex gap-[16px] bookInfo__wrapper">
+              <div className="w-full bookInfo__innerbook">
+                <div className="text-[#032b41] mb-[16px] font-[600] text-[32px] bookInfo__title">
                   {bookData.title}{" "}
                   {bookData.subscriptionRequired === true && (
                     <span>(Premium)</span>
                   )}
                 </div>
-                <div className="text-[#032b41] mb-[16px] font-[600]">
+                <div className="text-[#032b41] mb-[16px] font-[600] bookInfo__author">
                   {bookData.author}
                 </div>
-                <div className="text-[20px] text-[#032b41] mb-[16px] font-[300]">
+                <div className="text-[20px] text-[#032b41] mb-[16px] font-[300] bookInfo__subtitle">
                   {bookData.subTitle}
                 </div>
                 <div className="border-y-[1px] border-solid border-[#e1e7ea] py-[16px] mb-[24px]">
@@ -116,7 +116,7 @@ export default function BookInfo() {
                   </button>
                 </div>
 
-                <div className="flex items-center gap-[8px] text-[#0365f2] font-medium cursor-pointer mb-[40px] text-[18px]">
+                <div className="flex items-center gap-[8px] text-[#0365f2] font-medium cursor-pointer mb-[40px] text-[18px] bookInfo__bookmark">
                   <div className="flex w-[20px] h-[20px]">
                     <FontAwesomeIcon
                       icon={faBookmark}
@@ -131,27 +131,27 @@ export default function BookInfo() {
                 </div>
 
                 <div className="flex flex-wrap gap-[16px] mb-[16px]">
-                  <div className="bg-[#f1f6f4] px-[16px] h-[48px] flex items-center cursor-not-allowed text-[#032b41] font-medium rounded-[4px]">
+                  <div className="bg-[#f1f6f4] px-[16px] h-[48px] flex items-center cursor-not-allowed text-[#032b41] font-medium rounded-[4px] bookInfo__booktag">
                     {bookData.tags[0]}
                   </div>
-                  <div className="bg-[#f1f6f4] px-[16px] h-[48px] flex items-center cursor-not-allowed text-[#032b41] font-medium rounded-[4px]">
+                  <div className="bg-[#f1f6f4] px-[16px] h-[48px] flex items-center cursor-not-allowed text-[#032b41] font-medium rounded-[4px] bookInfo__booktag">
                     {bookData.tags[1]}
                   </div>
                 </div>
 
-                <div className="text-[#032b41] mb-[16px] leading-6">
+                <div className="text-[#032b41] mb-[16px] leading-6 bookInfo__description">
                   {bookData.bookDescription}
                 </div>
 
                 <div className="text-[18px] text-[#032b41] mb-[16px] font-semibold">
                   About the author
                 </div>
-                <div className="text-[#032b41] leading-6">
+                <div className="text-[#032b41] leading-6 bookInfo__description">
                   {bookData.authorDescription}
                 </div>
               </div>
 
-              <div>
+              <div className="bookInfo__pic">
                 <figure className="h-[300px] w-[300px] min-w-[300px]">
                   <img
                     src={bookData.imageLink}
