450bf5936a1d719a562873d8567b3d1a95655610
Ishaq-H <itzizak@hotmail.com>
2023-11-06 10:54:02 +0000

    .

--- Stripe/usePremiumStatus.jsx
+++ Stripe/usePremiumStatus.jsx
@@ -8,22 +8,16 @@ export default function usePremiumStatus(user) {
   const dispatch = useDispatch();
   const premiumStatus = useSelector((state) => state.user.premium);
 
-  useEffect(
-    () => {
-      return onIdTokenChanged(auth, async (user) => {
-        if (user) {
-          const tokenResult = await user.getIdTokenResult();
-          dispatch(
-            setPremiumStatus(tokenResult.claims.stripeRole === "premium")
-          );
-        } else {
-          dispatch(setPremiumStatus(false));
-        }
-      });
-    },
-    [user],
-    dispatch
-  );
+  useEffect(() => {
+    return onIdTokenChanged(auth, async (user) => {
+      if (user) {
+        const tokenResult = await user.getIdTokenResult();
+        dispatch(setPremiumStatus(tokenResult.claims.stripeRole === "premium"));
+      } else {
+        dispatch(setPremiumStatus(false));
+      }
+    });
+  }, [user]);
 
   return premiumStatus;
 }
